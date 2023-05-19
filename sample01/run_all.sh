b6f5a7569933bdfef0e6410b586e8c05f0651dd3
Baris Erkus <bariserkus@gmail.com>
2023-05-19 04:40:55 +0300

    New Build Config

--- sample01/run_all.sh
+++ sample01/run_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-TARGETS=("vina_A" "vina_B" "vina_C")
+TARGETS=("vina_A" "vina_B" "vina_C" "vina_D")
 
 CONF_FILE="conf.txt"
-CORES=("1" "4" "8" "12")
+CORES=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12")
 OUT_FILE="results.txt"
 
 for TARGET in ${TARGETS[@]};

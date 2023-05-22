f7ca659f8f19654ef0d33bf407584439d5d8a299
Baris Erkus <bariserkus@gmail.com>
2023-05-22 11:59:51 +0300

    Update full runs v2

--- build/linux/debug/run_all.sh
+++ build/linux/debug/run_all.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+CPU=$(uname -p)
+
 TARGETS=("vina_A" "vina_B" "vina_C" "vina_D")
 
 CONF_FILE="conf.txt"
 CORES=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12")
-OUT_FILE="results.txt"
+X86_VINA_OUT_FILE="x86_vina_out.txt"
+RISCV_VINA_OUT_FILE="riscv_vina_out.txt"
+
+X86_TIME_OUT_FILE="x86_runs.txt"
+RISCV_TIME_OUT_FILE="riscv_runs.txt"
 
 VEHAVE_PARAMS="VEHAVE_DEBUG_LEVEL=0 VEHAVE_VECTOR_LENGTH=16384"
 VEHAVE="vehave"
@@ -13,10 +19,10 @@ X86_64="x86_64"
 
 if [[ ${CPU} == ${X86_64} ]]; then
   for TARGET in ${TARGETS[@]}; do
-    for CORE in ${CORES[@]}; 	do
+    for CORE in ${CORES[@]}; do
       echo ${CPU}
-      echo ${TARGET} --config ${CONF_FILE} --out x86_runs.txt --cpu ${CORE} --out_time ${OUT_FILE}
-      ${TARGET} --config ${CONF_FILE} --out x86_runs.txt --cpu ${CORE} --out_time ${OUT_FILE}
+      echo ${TARGET} --config ${CONF_FILE} --out ${X86_VINA_OUT_FILE} --cpu ${CORE} --out_time ${X86_TIME_OUT_FILE}
+#      ${TARGET} --config ${CONF_FILE} --out ${X86_VINA_OUT_FILE} --cpu ${CORE} --out_time ${X86_TIME_OUT_FILE}
     done
   done
 elif [[ ${CPU} == ${RISCV} ]]; then
@@ -24,12 +30,12 @@ elif [[ ${CPU} == ${RISCV} ]]; then
     for CORE in ${CORES[@]}; do
       echo ${CPU}
       if [[ ${TARGET} == TARGET[0] ]]; then
-        echo ${TARGET} --config ${CONF_FILE}  --out riscv_runs.txt--cpu ${CORE} --out_time ${OUT_FILE}
-        ${TARGET} --config ${CONF_FILE} --out riscv_runs.txt --cpu ${CORE} --out_time ${OUT_FILE}
+        echo ${TARGET} --config ${CONF_FILE}  --out ${RISCV_TIME_OUT_FILE} --cpu ${CORE} --out_time ${X86_TIME_OUT_FILE}
+#        ${TARGET} --config ${CONF_FILE} --out ${RISCV_TIME_OUT_FILE} --cpu ${CORE} --out_time ${X86_TIME_OUT_FILE}
       else
-        echo ${VEHAVE_PARAMS} ${VEHAVE} ${TARGET} --config ${CONF_FILE}  --out riscv_runs.txt--cpu ${CORE} --out_time ${OUT_FILE}
-        ${VEHAVE_PARAMS} ${VEHAVE} ${TARGET} --config ${CONF_FILE} --out riscv_runs.txt --cpu ${CORE} --out_time ${OUT_FILE}
+        echo ${VEHAVE_PARAMS} ${VEHAVE} ${TARGET} --config ${CONF_FILE} --out ${RISCV_TIME_OUT_FILE} --cpu ${CORE} --out_time ${X86_TIME_OUT_FILE}
+#        ${VEHAVE_PARAMS} ${VEHAVE} ${TARGET} --config ${CONF_FILE} --out ${RISCV_TIME_OUT_FILE} --cpu ${CORE} --out_time ${X86_TIME_OUT_FILE}
       fi
-  	done
+    done
   done
 fi

9d4a514296d5aea3e66514f2f84794b0fb7f587b
Baris Erkus <bariserkus@gmail.com>
2023-05-23 01:25:21 +0300

    X86 runs complete

--- build/linux/debug/run_all.sh
+++ build/linux/debug/run_all.sh
@@ -2,9 +2,10 @@
 
 CPU=$(uname -p)
 
-X86_TARGETS=("vina_A" "vina_B" "vina_C" "vina_D")
-#RISCV_TARGETS=("vina_A" "vina_B" "vina_C" "vina_D")
-RISCV_TARGETS=("vina_E")
+X86_TARGETS=("vina_A" "vina_B" "vina_C" "vina_D" "vina_E")
+#X86_TARGETS=("vina_E")
+RISCV_TARGETS=("vina_A" "vina_B" "vina_C" "vina_D" "vina_E")
+#RISCV_TARGETS=("vina_E")
 
 CONF_FILE="conf.txt"
 X86_CORES=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12")

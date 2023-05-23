cd940652688de64d5d2edb841ed55b6aaffad880
Baris Erkus <bariserkus@gmail.com>
2023-05-23 11:39:43 +0300

    Fixed single runs

---
+++ build/linux/debug/run_single.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+CPU=$(uname -p)
+
+X86_TARGETS=("vina_As")
+RISCV_TARGETS=("vina_As")
+
+CONF_FILE="conf.txt"
+X86_CORES=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12")
+RISCV_CORES=("1" "2" "3" "4")
+
+X86_TIME_OUT_FILE="x86_single.txt"
+RISCV_TIME_OUT_FILE="riscv_single.txt"
+
+RISCV="riscv64"
+X86_64="x86_64"
+
+#Vehave Parameters
+VDL="0"
+VVL="16384"
+
+if [[ ${CPU} == ${X86_64} ]]; then
+  for TARGET in ${X86_TARGETS[@]}; do
+    for CORE in ${X86_CORES[@]}; do
+      echo ${CPU}
+      X86_VINA_OUT_FILE="X86_${TARGET}_${CORE}.txt"
+      echo ${TARGET} --config ${CONF_FILE} --out ${X86_VINA_OUT_FILE} --cpu ${CORE} --out_time ${X86_TIME_OUT_FILE}
+           ${TARGET} --config ${CONF_FILE} --out ${X86_VINA_OUT_FILE} --cpu ${CORE} --out_time ${X86_TIME_OUT_FILE}
+    done
+  done
+elif [[ ${CPU} == ${RISCV} ]]; then
+  for TARGET in ${RISCV_TARGETS[@]}; do
+    for CORE in ${RISCV_CORES[@]}; do
+      echo ${CPU}
+      RISCV_VINA_OUT_FILE="riscv_${TARGET}_${CORE}.txt"
+      VFILE="vehave_${TARGET}_${CORE}.trace"
+      echo VEHAVE_DEBUG_LEVEL=${VDL} VEHAVE_VECTOR_LENGTH=${VVL} VEHAVE_TRACE_FILE=${VFILE} vehave ${TARGET} --config ${CONF_FILE} --out ${RISCV_VINA_OUT_FILE} --cpu ${CORE} --out_time ${RISCV_TIME_OUT_FILE}
+           VEHAVE_DEBUG_LEVEL=${VDL} VEHAVE_VECTOR_LENGTH=${VVL} VEHAVE_TRACE_FILE=${VFILE} vehave ${TARGET} --config ${CONF_FILE} --out ${RISCV_VINA_OUT_FILE} --cpu ${CORE} --out_time ${RISCV_TIME_OUT_FILE}
+    done
+  done
+fi
